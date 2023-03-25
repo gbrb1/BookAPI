70537dd3169fb3600f596a91a5eccbf374d2e263
Gabriel Brum <gbrb1@hotmail.com>
2023-03-25 20:40:31 -0300

    Commit Inicial

---
+++ Controllers/BookController.cs
@@ -0,0 +1,62 @@
+﻿using BookAPI.Model;
+using BookAPI.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata.Ecma335;
+
+namespace BookAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookRepository _bookRepository;
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetAll()
+        {
+            return await _bookRepository.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Book>> Get(int id)
+        {
+            return await _bookRepository.GetBookById(id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Book>> PostBooks([FromBody] Book book)
+        {
+            var newBook = await _bookRepository.Create(book);
+            return CreatedAtAction(nameof(GetAll), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Book>> Delete(int id)
+        {
+            var bookToDelete = await _bookRepository.GetBookById(id);
+            if (bookToDelete != null)
+            {
+                await _bookRepository.Delete(bookToDelete.Id);
+                return NoContent();
+            }
+            return NotFound();
+
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<Book>> PutBooks(int id, [FromBody] Book book)
+        {
+            if (id == book.Id)
+            {
+                await _bookRepository.Update(book);
+                return NoContent();
+            }
+            return BadRequest();
+        }
+
+    }
+}
---
+++ Repositories/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using BookAPI.Model;
+
+namespace BookAPI.Repositories
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> Get();
+        Task<Book> GetBookById(int Id);
+        Task<Book> Create(Book book);
+        Task<Book> Update(Book book);
+        Task Delete(int Id);
+
+
+    }
+}
